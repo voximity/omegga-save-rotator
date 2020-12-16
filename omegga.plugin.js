6baf3574a122de0ee28e10a10917452bbd4f2f1a
voximity <zander@zanderf.net>
2020-12-15 19:40:38 -0600

    Add nominate, vote, and maps commands for nomination/voting of maps instead of random selection

--- omegga.plugin.js
+++ omegga.plugin.js
@@ -11,24 +11,55 @@ class SaveRotator {
         this.saves = this.saves.map(s => [s, Math.random()]).sort((a, b) => a[1] < b[1]).map(s => s[0]);
     }
 
+    async selectNextMap() {
+        if (this.config["enable-map-voting"]) {
+            // Map voting is enabled, so compile the list, broadcast, and wait for votes to be accumulated
+            this.shuffleSaves();
+
+            const allCandidates = Object.values(this.nominations).concat(this.saves);
+            this.votingCandidates = allCandidates.filter((s, i) => allCandidates.indexOf(s) == i).slice(0, this.config["map-candidate-count"]);
+
+            this.omegga.broadcast(`Voting will occur in ${this.config["voting-time"]} seconds! <b>Vote now using \`!vote #\`!`);
+            for (const i in this.votingCandidates)
+                this.omegga.broadcast(`${i * 1 /* this is precisely why javascript is cancerous */ + 1}) <b>${this.votingCandidates[i]}</b>`);
+
+            this.voting = true;
+            await new Promise(resolve => setTimeout(resolve, this.config["voting-time"] * 1000));
+            this.voting = false;
+
+            const winner = this.votingCandidates.map((c, i) => [c, Object.values(this.votes).filter(v => v - 1 == i).length]).sort((a, b) => b[1] - a[1])[0][0];
+            this.omegga.broadcast(`<b>${winner}</b> has been selected!`);
+
+            return winner;
+        } else {
+            // Map voting is not enabled, so just continue down the shuffled list
+            this.mapIndex++;
+            if (this.mapIndex >= this.saves.length) {
+                this.mapIndex = 0;
+                this.shuffleSaves();
+            }
+
+            return this.saves[this.mapIndex];
+        }
+    }
+
     async rotateMap(callerIsInterval) {
         // Refresh the interval timer if the caller wasn't the timer, and we're actually using an interval to control map rotation (e.g. when !rtv is run mid-way thru waiting for map rotation)
         if (!callerIsInterval && this.interval != undefined)
             this.interval.refresh();
 
+        const selectedMap = await this.selectNextMap();
+
+        this.votes = {};
+        this.nominations = {};
         this.voters = [];
         this.omegga.clearAllBricks(true); // boolean will matter in a5
-        this.mapIndex++;
-        if (this.mapIndex >= this.saves.length) {
-            this.mapIndex = 0;
-            this.shuffleSaves();
-        }
 
         // Wait after clearing if the field is greater than zero
         if (this.config["wait-after-clearing"] > 0)
             await new Promise(resolve => setTimeout(resolve, this.config["wait-after-clearing"] * 1000));
 
-        this.omegga.loadBricks(this.saves[this.mapIndex]);
+        this.omegga.loadBricks(selectedMap);
     }
 
     checkVoters() {
@@ -55,7 +86,10 @@ class SaveRotator {
             }, this.config["rotate-on-interval"] * 1000);
         }
 
+        this.nominations = {}; // <username>: <name of map nominated>
+        this.votes = {}; // <username>: <index of map candidate voted for>
         this.voters = [];
+        this.voting = false;
         this.mapIndex = -1; // Initially below zero as the map needs to be rotated once to correctly pass through a full shuffle
 
         this.omegga.on("leave", async (name) => {
@@ -89,6 +123,34 @@ class SaveRotator {
                 // The user has voted, remove them
                 this.voters.splice(index, 1);
             }
+        });
+
+        this.omegga.on("chatcmd:nominate", async (name, ...args) => {
+            if ((!this.config["enable-map-voting"] || !this.config["enable-map-nominations"]) && !this.voting) return;
+
+            const mapName = args.join(" ");
+
+            if (this.saves.includes(mapName)) {
+                this.nominations[name] = mapName;
+                this.omegga.broadcast(`<b>${name}</b> has nominated the map <b>${mapName}</b>!`);
+            } else {
+                this.omegga.broadcast(`<b>${mapName}</b> is not a valid map! Map names are case-sensitive. View all with \`!maps\`.`);
+            }
+        });
+
+        this.omegga.on("chatcmd:vote", async (name, candidateIndex) => {
+            if (!this.config["enable-map-voting"] || !this.voting) return;
+
+            if (candidateIndex >= 1 && candidateIndex <= this.votingCandidates.length) {
+                this.votes[name] = candidateIndex;
+                this.omegga.broadcast(`<b>${name}</b> has voted for <b>${this.votingCandidates[candidateIndex - 1]}</b>!`);
+            } else {
+                this.omegga.broadcast("Invalid map index!");
+            }
+        });
+
+        this.omegga.on("chatcmd:maps", async (name) => {
+            this.omegga.broadcast(this.saves.join(", "));
         });
     }
 
